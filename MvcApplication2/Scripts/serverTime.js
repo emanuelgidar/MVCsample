f613625612fe7ed6293c13147a974a6c5cc79203
unknown <cristian.gidar@gmail.com>
2013-09-24 15:42:59 +0300

    asd
    Signed-off-by: unknown <cristian.gidar@gmail.com>

---
+++ MvcApplication2/Scripts/serverTime.js
@@ -0,0 +1,20 @@
+﻿window.onload = function () {
+    var link = document.getElementById("timeLink");
+    link.onclick = getServerTime();
+}
+
+function getServerTime() {
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "/Home/GetServerTime");
+
+    xhr.onreadystatechange = function () {
+         if (xhr.readyState == 4) {
+           if (xhr.status == 200) {
+        var timeDiv = document.getElementById("timeDisplay");
+        timeDiv.innerHTML = xhr.responseText;
+
+             }
+         }
+        xhr.send();
+    }
+}
